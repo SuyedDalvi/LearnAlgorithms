f93ae48c66da5a30d437b6ee4a07db312fdce91f
SuyedDalvi <suyed786@gmail.com>
2021-05-21 13:59:31 -0400

    learning recursion

--- leetcode/LeetcodeProblems/python/reverse.py
+++ leetcode/LeetcodeProblems/python/reverse.py
@@ -4,4 +4,18 @@ def reverse(x: int) -> int:
     else:
         return x%10 + reverse(x/10)
 
-print(reverse(123))
+# print(reverse(123))
+
+
+#  Reversing String using Recursion
+def helper(index, str):    
+    if( str == None or index == len(str)):
+        print(index)
+        return ''
+    return helper(index + 1, str) + str[index]
+    
+def recReverse(str):
+    reverse = helper(0, str)
+    print(reverse)
+
+recReverse('str')
--- leetcode/LeetcodeProblems/src/App.java
+++ leetcode/LeetcodeProblems/src/App.java
@@ -2,5 +2,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         RomanToInt romanToint = new RomanToInt();
         System.out.println(romanToint.romanToInt("MCMXCIV"));
+        App.printReverse("str".toCharArray());
     }
+
+    private static void printReverse(char [] str) {
+        helper(0, str);
+      }
+      
+      private static void helper(int index, char [] str) {
+        if (str == null || index >= str.length) {
+            System.out.println(index);
+          return;
+        }
+        helper(index + 1, str);
+        System.out.print(str[index]);
+      }
 }
